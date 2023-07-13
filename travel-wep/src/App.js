bc4cfc7b3a7ac6ea00323816f95dd93e24b4873c
aws69 <awsalomari11469@gmail.com>
2023-07-13 15:12:11 +0300

    lab 17

--- travel-wep/src/App.js
+++ travel-wep/src/App.js
@@ -6,17 +6,17 @@ import Footer from "./components/footer/Footer";
 import TourDetails from './components/TourDetails/TourDetails';
 
 import data from "./data/db.json";
-import Navbar from './components/Navbar/Navbar';
+
 
 
 function App() {
   return (
     <div className='App'>
-      <Navbar />
+      
       <Routes>
         <Route path='/' element={<Home DATA={data} />} />
         <Route path='/city/:id' element={<TourDetails DATA={data} />} />
-        <Footer />
+        <Route path='/footer' element={ <Footer />} />
       </Routes>
 
     </div>
